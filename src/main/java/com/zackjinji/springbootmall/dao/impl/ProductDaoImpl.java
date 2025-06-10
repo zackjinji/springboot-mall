c0b8da736d2199888ebc02720e2db2a087c474a1
zackjinji <zackjinji@gmail.com>
2025-06-10 15:17:44 +0800

    商品功能 - 實作新增商品

---
+++ src/main/java/com/zackjinji/springbootmall/dao/impl/ProductDaoImpl.java
@@ -0,0 +1,71 @@
+package com.zackjinji.springbootmall.dao.impl;
+
+import com.zackjinji.springbootmall.dao.ProductDao;
+import com.zackjinji.springbootmall.dto.ProductRequest;
+import com.zackjinji.springbootmall.model.Product;
+import com.zackjinji.springbootmall.rowmapper.ProductRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class ProductDaoImpl implements ProductDao {
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+
+    @Override
+    public Product getProductById(Integer productId) {
+        String sql = "SELECT product.product_id, product_name, category, image_url, price, stock, description, " +
+                "created_date, last_modified_date " +
+                "FROM product WHERE product_id = :productId";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("productId", productId);
+
+        List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
+
+        if(productList.size() > 0){
+            return productList.get(0);
+        }   else {
+            return null;
+        }
+
+    }
+
+    @Override
+    public Integer createProduct(ProductRequest productRequest) {
+        String sql = "INSERT INTO product(product_name, category, image_url, price, stock, description, created_date, last_modified_date) " +
+                "VALUES (:product_name, :category, :imageUrl, :price, :stock, :description, :createdDate, :lastModifideDate)";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("product_name", productRequest.getProductName());
+        map.put("category", productRequest.getCategory().toString());
+        map.put("image_url", productRequest.getImageUrl());
+        map.put("price", productRequest.getPrice());
+        map.put("stock", productRequest.getStock());
+        map.put("description", productRequest.getDescription());
+
+        Date now = new Date();
+        map.put("createdDate", now);
+        map.put("lastModifiedDate", now);
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(map), keyHolder);
+
+        int productId = keyHolder.getKey().intValue();
+
+        return productId;
+
+
+    }
+}
